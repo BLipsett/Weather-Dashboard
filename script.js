15cbf655f9c088f501864cdc7e6a4fc5ebe66c48
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-02-04 21:45:20 -0500

    running api

---
+++ script.js
@@ -0,0 +1,54 @@
+_dsSecret = "&KEY=8847349d953145628ca0bbd65ed8bb9c";
+
+//query for dark sky api
+function buildQuery(city) {
+
+
+
+    //https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+    let queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + city + _dsSecret;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response);
+        let apiKey = _dsSecret;
+        //let city = "#search-term";
+        let getWeb = queryURL + apiKey;// + (zipCode());
+        console.log(city);
+
+
+
+        //Build HTML for generated information
+        let tempF = ((response.data[0].temp) * 9 / 5) + 32;
+        let cityName = $("<h1>").text(response.city_name);
+        let date = $("<h4>").text(response.data[0].valid_date);
+        cityName.append(date);
+        let temp = $("<h4>").append(tempF) + "F";
+        let weather = $("<img>").attr(response.data[0].weather.icon);
+        let condition = $("<div>").text(response.data[0].weather.description);
+        condition.append(weather);
+        let curWeather = $("<div>");
+        curWeather.append(cityName, temp, condition);
+
+        $("#curWeather").append(curWeather);
+        //$("#article-section").append(cityName, temp, condition);
+//if (response === )
+        return getWeb;
+    });
+}
+
+/*function zipCode() {
+    let zip = $("term");
+
+    let geoLat = zip;
+    let geoLong = newZip;
+    
+    
+}*/
+$("#run-search").on("click", function () {
+    event.preventDefault();
+    let city = $("#search-term").val().trim();
+    buildQuery(city);
+});
